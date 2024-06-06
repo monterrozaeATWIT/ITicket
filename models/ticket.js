f9749acec5794abdfcfd538b9ff1c0e139fb9799
Elmer Monterroza <monterrozae@wit.edu>
2024-06-06 15:29:09 -0400

    Update ticket.js

--- models/ticket.js
+++ models/ticket.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const ticketSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String },
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   status: { type: String, default: 'open' }
 });
 
