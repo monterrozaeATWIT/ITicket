9bfd9677c9707a80e820a860baacc25a589e829d
Elmer Monterroza <monterrozae@wit.edu>
2024-06-11 17:18:25 -0400

    Update ticket.js

--- models/ticket.js
+++ models/ticket.js
@@ -1,11 +1,18 @@
 const mongoose = require('mongoose');
+const { v4: uuidv4 } = require('uuid')
 
+// Template for ticket
 const ticketSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  id: { type: String, required: true },
-  created: { type: Date, default: Date.now },
+  id: { type: String, default: uuidv4, unique: true, required: true},
+  created: { type: Date, default: Date.now }, 
   description: { type: String },
+  priority: { type: String },
+  department: { type: String, required: true },
   status: { type: String, default: 'open' }
+  // Todo: add username field
 });
 
-module.exports = mongoose.model('Ticket', ticketSchema);
+const Ticket = mongoose.model('Ticket', ticketSchema);
+
+module.exports = Ticket;
