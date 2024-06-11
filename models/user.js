9f12c38605185b3627d40dd5cc0028af90931668
Elmer Monterroza <monterrozae@wit.edu>
2024-06-11 17:22:14 -0400

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 
+// Template for new user
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true }
+  // Todo: add department field (and password field?)
 });
 
 module.exports = mongoose.model('User', userSchema);
