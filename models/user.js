45b7063ae3541d3ca3e23a56dafd07673a445163
Elmer Monterroza <monterrozae@wit.edu>
2024-06-16 20:10:55 -0400

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,10 +1,26 @@
 const mongoose = require('mongoose');
 
-// Template for new user
+// Import bycrypt to add hashing function to store passwords securely 
+const bcrypt = require('bcrypt');
+
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true }
-  // Todo: add department field (and password field?)
+  email: { type: String, required: true, unique: true },
+  department: {type: String },
+  password: { type: String, required: true }
+
 });
+
+// Hash the password before storing on MongoDB with 10 rounds of salt
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// Compare given password with the database hash with bcrypt
+userSchema.methods.comparePassword = function(password) {
+  return bcrypt.compare(password, this.password);
+};
 
 module.exports = mongoose.model('User', userSchema);
