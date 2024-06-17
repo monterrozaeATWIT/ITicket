d9a350677323312724bae766ac37e9df948349dd
Elmer Monterroza <monterrozae@wit.edu>
2024-06-16 20:11:55 -0400

    Update ticket.js

--- models/ticket.js
+++ models/ticket.js
@@ -4,7 +4,7 @@ const { v4: uuidv4 } = require('uuid')
 // Template for ticket
 const ticketSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  id: { type: String, default: uuidv4, unique: true, required: true},
+  id: { type: String, default: uuidv4, unique: true, required: true}, // Create a new unique ID for the ticket
   created: { type: Date, default: Date.now }, 
   description: { type: String },
   priority: { type: String },
