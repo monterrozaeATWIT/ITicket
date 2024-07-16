d8da28da88250b60ab899b14a667aa5376bea5ce
Elmer Monterroza <monterrozae@wit.edu>
2024-07-16 16:10:57 -0400

    Update ticket.js

--- models/ticket.js
+++ models/ticket.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const { v4: uuidv4 } = require('uuid')
 const ticketSchema = new mongoose.Schema({
+    ticketID: { type: String, default: generateTicketId, unique: true },
     department: { type: String, required: true },
     priority: { type: String, required: true },
     subject: { type: String, required: true },
@@ -10,5 +11,9 @@ const ticketSchema = new mongoose.Schema({
     userEmail: { type: String, required: true },
     status: { type: String, enum: ['Open', 'Working on it', 'Closed'], default: 'Open' } // New status field
 });
+
+function generateTicketId() {
+  return Math.floor(10000 + Math.random() * 90000).toString(); // Generates a 5-digit number
+}
 
 module.exports = mongoose.model('Ticket', ticketSchema);
